2e4a11014a08a5af26dd8b60e0396ece41b6347b
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-07-19 20:23:11 -0300

    feat: #1000 - Hello World! (Java)

---
+++ 1_iniciante_beginner/java/1000.java
@@ -0,0 +1,18 @@
+/**
+ * @source Beecrowd
+ * @problem 1000 - Hello World!
+ * @subject Sequential
+ * 
+ * @author Letícia Cruz
+ * @date 2025-04-18
+ */
+
+import java.io.IOException; // uso padrão no beecrowd para java
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello World!");// imprime Hello World!
+    }
+}
