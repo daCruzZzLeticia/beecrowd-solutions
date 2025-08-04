51727d715eec29c55aa54955bd95b6d0905a69a8
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-08-04 18:25:05 -0300

    feat: #1001 - Extremely Basic (Python)

---
+++ 1_iniciante_beginner/python/1001.py
@@ -0,0 +1,15 @@
+"""
+@source Beecrowd
+@problem 1001 - Extremely Basic
+@subject Sequential
+
+@author Letícia Cruz
+@date 2024-04-03
+"""
+
+A = int(input())
+B = int(input())
+
+soma = A+B
+
+print("X =", soma)
