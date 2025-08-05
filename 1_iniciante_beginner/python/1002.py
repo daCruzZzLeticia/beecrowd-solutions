c8a100d7767f81a1dcd6e4757dba70472397bf51
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-08-05 08:48:36 -0300

    feat: #1002 - Area of a Circle (Python)

---
+++ 1_iniciante_beginner/python/1002.py
@@ -0,0 +1,15 @@
+"""
+@source Beecrowd
+@problem 1002 - Area of a Circle
+@subject Sequential
+
+@author Letícia Cruz
+@date 2024-04-03
+"""
+
+pi = 3.14159
+raio = float(input())
+
+area = raio*raio*pi
+
+print(f'A={area:.4f}')
