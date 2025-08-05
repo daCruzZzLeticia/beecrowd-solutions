11f283eec9f27f02f8b8548f29888a6181db0488
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-08-05 12:08:16 -0300

    feat: #2374 - Tire (Python)

---
+++ 2_ad_hoc/python/2374.py
@@ -0,0 +1,15 @@
+"""
+@source Beecrowd
+@problem 2374 - Tire
+@subject Unknown
+
+@author Letícia Cruz
+@date 2025-07-14
+"""
+
+n = int(input())
+m = int(input())
+
+diferenca = n-m
+
+print(diferenca)
