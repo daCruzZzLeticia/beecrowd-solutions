52f0169f0bd7bc7d661404ae9271366091c758a4
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-08-07 12:48:36 -0300

    feat: #1005 - Average 1 (Python)

---
+++ 1_iniciante_beginner/python/1005.py
@@ -0,0 +1,15 @@
+"""
+@source Beecrowd
+@problem 1005 - Average 1
+@subject Sequential
+
+@author Letícia Cruz
+@date 2025-05-14
+"""
+
+A = float(input())
+B= float(input())
+
+MEDIA = (A * 3.5 + B * 7.5) / 11;
+
+print(f'MEDIA = {MEDIA:.5f}')
