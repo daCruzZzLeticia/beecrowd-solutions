3206c2ee8a7474d0036ed56c6167c04413ad2c5a
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-08-07 12:43:42 -0300

    feat: #1004 - Simple Product (Python)

---
+++ 1_iniciante_beginner/python/1004.py
@@ -0,0 +1,16 @@
+"""
+@source Beecrowd
+@problem 1004 - Simple Product
+@subject Sequential
+
+@author Letícia Cruz
+@date 2025-05-07
+"""
+
+# entradas
+A = int(input())
+B = int(input())
+
+PROD = A*B # cálculo do produto
+
+print("PROD =", PROD) # imprime produto
