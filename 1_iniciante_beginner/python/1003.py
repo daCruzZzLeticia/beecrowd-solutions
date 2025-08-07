a885344d97b36fea368c9807147e44b7f8cab03f
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-08-07 12:39:15 -0300

    feat: #1003 - Simple Sum (Python)

---
+++ 1_iniciante_beginner/python/1003.py
@@ -0,0 +1,15 @@
+"""
+@source Beecrowd
+@problem 1003 - Simple Sum
+@subject Sequential
+
+@author Letícia Cruz
+@date 2025-05-02
+"""
+
+ = int(input())
+B = int(input())
+
+SOMA = A+B
+
+print("SOMA =", SOMA)
