7386aff8ce43abe198407ab3efaef03c33e9e41e
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-08-16 14:28:11 -0300

    feat: #2602 - Basic Select (SQL)

---
+++ 7_sql/2602.sql
@@ -0,0 +1,10 @@
+/* - - - - - - - - - - - - - - -
+@source Beecrowd
+@problem 2602 - Basic Select
+@subject Data Selection
+
+@author Letícia Cruz
+@date 2021-10-26
+- - - - - - - - - - - - - - - */
+
+select name from customers where state = 'RS';
