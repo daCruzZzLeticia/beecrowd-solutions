c06d362f5667e60751c3e05e963aaa5ff435a2f9
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-08-16 17:49:03 -0300

    feat: #2603 - Customer Address (SQL)

---
+++ 7_sql/2603.sql
@@ -0,0 +1,10 @@
+/* - - - - - - - - - - - - - - -
+@source Beecrowd
+@problem 2603 - Customer Address
+@subject Data Selection
+
+@author Letícia Cruz
+@date 2021-10-26
+- - - - - - - - - - - - - - - */
+
+select name, street from customers where city = 'Porto Alegre';
