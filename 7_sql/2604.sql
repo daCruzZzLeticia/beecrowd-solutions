0fa834c1e01a561ffd39d577b4d816235190d5c8
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-08-16 17:51:40 -0300

    feat: #2604 - Under 10 or Greater Than 100 (SQL)

---
+++ 7_sql/2604.sql
@@ -0,0 +1,10 @@
+/* - - - - - - - - - - - - - - -
+@source Beecrowd
+@problem 2604 - Under 10 or Greater Than 100
+@subject Data Selection
+
+@author Letícia Cruz
+@date 2021-10-26
+- - - - - - - - - - - - - - - */
+
+select id, name from products where price < 10 or price > 100;
